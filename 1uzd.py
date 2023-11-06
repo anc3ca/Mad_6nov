039dffd8d69e297d73a343ad46ee636cb7863e75
anc3ca <aa.caune@gmail.com>
2023-11-06 09:13:24 +0200

    izpildiju pirmo uzdevumu

--- 1uzd.py
+++ 1uzd.py
@@ -1,4 +1,9 @@
 """
 Ja vēlaties iegūt nejaušu skaitli no 1 līdz 10.
 
+jābut iespejai izdrukāt datora izvēlētu skaitli robežās no 1 līdz 10
 """
+import random 
+nejauss_skaitlis = random.randint(1, 10)
+
+print(nejauss_skaitlis)
